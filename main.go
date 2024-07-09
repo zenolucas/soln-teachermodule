d81ad3fc66636cc1aca9fff3fff91cd51f9d4ed1
Zen Lucas <2200501@slu.edu.ph>
2024-07-09 13:37:06 +0800

    create initial routes

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"embed"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"soln-teachermodule/handler"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
+
+//go:embed public
+var FS embed.FS
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -16,8 +21,12 @@ func main() {
 	}
 
 	router := chi.NewMux()
+	router.Use(handler.WithUser)
 
-	// router.Get("/", )
+	// handle static files
+	router.Handle("/*", http.StripPrefix("/", http.FileServer(http.FS(FS))))
+	router.Get("/", handler.Make(handler.HandleHomeIndex))
+	router.Get("/login", handler.Make(handler.HandleLoginIndex))
 
 	port := os.Getenv("HTTP_LISTEN_ADDRESS")
 	slog.Info("application running", "port", port)
