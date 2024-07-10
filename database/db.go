32bcaa951b34303b470870ddc01edd837673295d
Zen Lucas <2200501@slu.edu.ph>
2024-07-10 10:03:30 +0800

    create db.go

---
+++ database/db.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"log"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var db *sql.DB
+
+func InitializeDatabase() error {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               os.Getenv("DBNAME"),
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Database connection established.")
+
+	return err
+}
+
+
