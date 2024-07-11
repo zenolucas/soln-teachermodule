34b58486860f86daa5743a3717173a8c981003ae
Zen Lucas <2200501@slu.edu.ph>
2024-07-11 12:54:35 +0800

    style auth.templ, and fix logo recursion problem

--- handler/auth.go
+++ handler/auth.go
@@ -2,11 +2,18 @@ package handler
 
 import (
 	"net/http"
+	"soln-teachermodule/database"
 	"soln-teachermodule/view/auth"
 )
 
 func HandleLoginIndex(w http.ResponseWriter, r *http.Request) error {
 
-	
-	return auth.Login().Render(r.Context(), w) 
+	return auth.Login().Render(r.Context(), w)
+}
+
+func HandleLoginCreate(w http.ResponseWriter, r *http.Request) error {
+	// authenticate the user
+	database.AuthenticateUser(w, r)
+
+	return auth.Login().Render(r.Context(), w)
 }
