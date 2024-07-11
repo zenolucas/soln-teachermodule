2360db26c2cc904de20a03d55e22a9f7345a4a5a
Zen Lucas <2200501@slu.edu.ph>
2024-07-11 15:55:17 +0800

    implement and style initial homepage.index

--- handler/auth.go
+++ handler/auth.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"soln-teachermodule/database"
 	"soln-teachermodule/view/auth"
+	"soln-teachermodule/view/home"
 )
 
 func HandleLoginIndex(w http.ResponseWriter, r *http.Request) error {
@@ -13,7 +15,12 @@ func HandleLoginIndex(w http.ResponseWriter, r *http.Request) error {
 
 func HandleLoginCreate(w http.ResponseWriter, r *http.Request) error {
 	// authenticate the user
-	database.AuthenticateUser(w, r)
+	err := database.AuthenticateUser(w, r)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		return home.Index().Render(r.Context(), w)
+	}
 
 	return auth.Login().Render(r.Context(), w)
 }
