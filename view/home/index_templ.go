f8495d725d3aacc17b7b8da39a89acc70ebec5c1
Zen Lucas <2200501@slu.edu.ph>
2024-09-17 11:18:34 +0800

    style navbar, classroom (sidebar, levels)

--- view/home/index_templ.go
+++ view/home/index_templ.go
@@ -40,7 +40,7 @@ func Index() templ.Component {
 			}
 			return templ_7745c5c3_Err
 		})
-		templ_7745c5c3_Err = layout.App(true, true).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.App(true).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
