385c6e5d155979e9a9eaf902963496d3f04b8b1d
Zen Lucas <2200501@slu.edu.ph>
2024-09-27 22:23:46 +0800

    update soln_db.sql

--- soln_db.sql
+++ soln_db.sql
@@ -1,8 +1,6 @@
--- Create the database
 CREATE DATABASE IF NOT EXISTS soln_db;
 USE soln_db;
 
--- Create the users table
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -10,66 +8,87 @@ CREATE TABLE IF NOT EXISTS users (
     usertype ENUM('student', 'teacher') NOT NULL
 );
 
--- Create the subjects table
-CREATE TABLE IF NOT EXISTS subjects (
-    subject_id INT AUTO_INCREMENT PRIMARY KEY,
-    subject_name VARCHAR(100) NOT NULL,
-    teacher_id INT,
+CREATE TABLE IF NOT EXISTS classrooms (
+    classroom_id INT AUTO_INCREMENT PRIMARY KEY,
+    classroom_name VARCHAR(100) NOT NULL,
+    section VARCHAR(100),
+    teacher_id INT NOT NULL,
     FOREIGN KEY (teacher_id) REFERENCES users(user_id)
 );
 
-
--- Create the enrollments table (for which students are assigned to each subject)
 CREATE TABLE IF NOT EXISTS enrollments (
     enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
-    subject_id INT,
+    classroom_id INT,
     student_id INT,
-    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id),
+    FOREIGN KEY (classroom_id) REFERENCES classrooms(classroom_id),
     FOREIGN KEY (student_id) REFERENCES users(user_id),
-    UNIQUE KEY unique_enrollment (subject_id, student_id)
+    UNIQUE KEY unique_enrollment (classroom_id, student_id)
 );
 
+CREATE TABLE IF NOT EXISTS levels (
+    level_id INT AUTO_INCREMENT PRIMARY KEY,
+    level_name VARCHAR(100)
+);
 
--- Create the MiniGames table
 CREATE TABLE IF NOT EXISTS minigames (
     minigame_id INT AUTO_INCREMENT PRIMARY KEY,
     minigame_name   VARCHAR(100) NOT NULL,
     minigame_type   ENUM('quest', 'miniboss') NOT NULL
 );
 
--- Create the Levels table
-CREATE TABLE IF NOT EXISTS levels (
-    level_id INT AUTO_INCREMENT PRIMARY KEY,
-    level_name VARCHAR(100)
-);
-
--- Create the LevelMiniGameConfig
-CREATE TABLE IF NOT EXISTS levelgames (
+CREATE TABLE IF NOT EXISTS level_minigames (
     level_id INT, 
     minigame_id INT, 
     FOREIGN KEY (level_id) REFERENCES levels(level_id),
     FOREIGN KEY (minigame_id) REFERENCES minigames(minigame_id)
 );
 
+CREATE TABLE IF NOT EXISTS questions (
+    question_id INT AUTO_INCREMENT PRIMARY KEY, 
+    minigame_id INT,
+    question_text   VARCHAR(200) NOT NULL,
+    correct_answer  VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS choices (
+    choice_id INT AUTO_INCREMENT PRIMARY KEY,
+    question_id INT, 
+    choice_text     VARCHAR(100) NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
+)
+
 -- Suggestions: 
 -- Superadmin and admin
-
 
 -- Insert initial data into users table
 INSERT INTO users (username, password, usertype) VALUES
 ('user1', 'pw', 'teacher'),
 ('user2', 'password_hash2', 'teacher'),
-('user3', 'password_hash3', 'student'),
+('user3', 'pw', 'student'),
 ('user4', 'password_hash4', 'student'),
 ('user5', 'password_hash5', 'student');
 
 -- Insert initial data into subjects table
-INSERT INTO subjects (subject_name, teacher_id) VALUES
-('Math101', 1),
-('G6-Mathematics', 2);
+INSERT INTO classrooms (classroom_name, section, teacher_id) VALUES
+('Math101', '1', 1),
+('Remedial Class 1', '2', 2);
 
 -- Insert initial data into enrollments table
-INSERT INTO enrollments (subject_id, student_id) VALUES 
+INSERT INTO enrollments (classroom_id, student_id) VALUES 
 (1, 1),
 (1, 2),
 (2, 3);
+
+INSERT INTO questions (minigame_id, question_text, correct_answer) VALUES 
+(1, 'What is 1/2 + 1/2 ?', '1')
+(1, 'What is 1/3 + 1/3 ?', '2/3');
+
+INSERT INTO choices (question_id, choice_text) VALUES 
+(1, '1/2'),
+(1, '1/3'),
+(1, '1'),
+(1, '2'),
+(2, '1/2'),
+(2, '1/3'),
+(2, '1/4'),
+(2, '2/3');
