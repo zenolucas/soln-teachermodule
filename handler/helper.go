c52a522db9ad6b1ee110b8f1fd85debd5abe91b6
Zen Lucas <2200501@slu.edu.ph>
2024-10-08 23:46:56 +0800

    fix redirecting bug with hxRedirect function

--- handler/helper.go
+++ handler/helper.go
@@ -22,8 +22,18 @@ func GetAuthenticatedUser(r *http.Request) types.AuthenticatedUser {
 		return types.AuthenticatedUser{}
 	}
 	return user
-} 
+}
 
 func render(w http.ResponseWriter, r *http.Request, component templ.Component) error {
 	return component.Render(r.Context(), w)
+}
+
+func hxRedirect(w http.ResponseWriter, r *http.Request, to string) error {
+	if len(r.Header.Get("HX-Request")) > 0 {
+		w.Header().Set("HX-Redirect", to)
+		w.WriteHeader(http.StatusSeeOther)
+		return nil
+	}
+	http.Redirect(w, r, to, http.StatusSeeOther)
+	return nil
 }
