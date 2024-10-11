08a08be70cf09799050e7554a709c898163d0a3b
Zen Lucas <2200501@slu.edu.ph>
2024-10-11 12:25:32 +0800

    refactor

--- handler/middleware.go
+++ handler/middleware.go
@@ -19,7 +19,7 @@ func WithAuth(next http.Handler) http.Handler {
 		store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
 		session, _ := store.Get(r, sessionUserKey)
 
-		fmt.Print("authenticatd is : ", session.Values["authenticated"])
+		// fmt.Print("authenticatd is : ", session.Values["authenticated"])
 
 		if !session.Values["authenticated"].(bool) {
 			fmt.Print("this is executed!")
@@ -32,21 +32,3 @@ func WithAuth(next http.Handler) http.Handler {
 	}
 	return http.HandlerFunc(fn)
 }
-
-// func WithAuth(next http.Handler) http.Handler {
-// 	fn := func(w http.ResponseWriter, r *http.Request) {
-// 		if strings.Contains(r.URL.Path, "/public") {
-// 			next.ServeHTTP(w, r)
-// 			return
-// 		}
-// 		fmt.Print("this is executed")
-// 		user := GetAuthenticatedUser(r)
-// 		if !user.LoggedIn {
-// 			path := r.URL.Path
-// 			http.Redirect(w, r, "/login?to="+path, http.StatusSeeOther)
-// 			return
-// 		}
-// 		next.ServeHTTP(w, r)
-// 	}
-// 	return http.HandlerFunc(fn)
-// }
