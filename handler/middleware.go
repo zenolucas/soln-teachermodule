8cb3c9066455245b88a3481847ab01f33c5f6a24
Zen Lucas <2200501@slu.edu.ph>
2024-11-12 23:40:00 +0800

    hope to fix bug that crashes server in ubuntu server

--- handler/middleware.go
+++ handler/middleware.go
@@ -2,10 +2,10 @@ package handler
 
 import (
 	"net/http"
-	"os"
+	// "os"
 	"strings"
 
-	"github.com/gorilla/sessions"
+	// "github.com/gorilla/sessions"
 )
 
 func WithAuth(next http.Handler) http.Handler {
@@ -15,17 +15,17 @@ func WithAuth(next http.Handler) http.Handler {
 			return
 		}
 
-		store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
-		session, _ := store.Get(r, sessionUserKey)
+		// COMMENTED OUT TO HOPEFULLY FIX BUG CRASH IN UBUNTU SERVER
+		// store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
+		// session, _ := store.Get(r, sessionUserKey)
+
+		// if !session.Values["authenticated"].(bool) {
+		// 	path := r.URL.Path
+		// 	hxRedirect(w, r, "/?to"+path)
+		// 	return
+		// }
 
 		// fmt.Print("authenticated is : ", session.Values["authenticated"])
-
-		if !session.Values["authenticated"].(bool) {
-			path := r.URL.Path
-			hxRedirect(w, r, "/?to"+path)
-			return
-		}
-
 		next.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
